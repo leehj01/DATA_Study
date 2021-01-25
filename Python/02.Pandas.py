8ed3026844b330b14aa23de0a077ba66d8fbd3e2
leehj01 <leehjung55@gmail.com>
2021-01-25 19:45:20 +0900

    commit

---
+++ Python/02.Pandas.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Drop - row, column 삭제 
+
+import numpy as np
+
+data = pd.DataFrame(np.arange(16).reshape((4,4,)),
+                   index = ['A','B','C','D'],
+                   columns = ['가','나','다','라'])
+data
+
+data.drop(['A','B'])
+
+data.drop(['가','나'], axis= 1)
+
+# # 데이터프레임 컬럼이름 변경하기
+
+data
+
+# ## 1) 기존 컬럼명 1:1 매칭으로 변경하기
+
+data = data.rename({'가':'도','나':'레'}, axis = 'columns')
+data
+
+# ## 새 컬럼명 직접 덮어쓰기
+
+data.columns = ['이','얼','싼']
+
+data.columns = ['z이','z얼','z싼','z쓰']
+data
+
+# ## 3) 기존문자를 대체 문자로 바꾸기
+
+data.columns = data.columns.str.replace('z','zh')
+data
+
+# ## 4) prefix 사용
+
+data.add_prefix('lang')
+
+# ## suffix 사용
+
+data.add_suffix('lang')
+
+# # 데이터프레임 컬럼 순서 변경
+
+data = pd.DataFrame(np.arange(16).reshape((4,4,)),
+                   index = ['A','B','C','D'],
+                   columns = ['나','다','가','라'])
+data
+
+data = data[['가','나','다','라']]
+data
+
+# # 데이터 프레임 새 컬럼 추가하기
+
+data['마'] = '한글'
+data
+
+data.loc[:, '바'] = '언어'
+data
+
+# # 인덱스 세팅과 재설정
+
+# ## 1) 배정을 통한 인덱스 설정
+
+data = pd.DataFrame(np.arange(16).reshape((4,4,)),
+                   index = ['A','B','C','D'],
+                   columns = ['가','나','다','라'])
+data
+
+# 덮어 쓸땐 길이가 동일 해야한다.
+data.index = ['a','b','c','d']
+data.columns = ['도','레','미','파']
+data
+
+# ## 2) dataframe내의 열을 이용한 인덱스 설정 (set_index 메서드 )
+
+# + active=""
+# data.set_index(
+#     keys,          # 인덱스로 바꾸려는 열을 선택
+#     drop=True,     # 인덱스로 세팅한 열을 dataframe내에서 삭제할지 결정
+#     append=False,  # 기존에 존재한 인덱스 삭제 여부 
+#     inplace=False, # 원본 객체를 변경할지 선택
+#     verify_integrity=False,
+# )
+# -
+
+data.set_index('도')
+
+# ## dataframe의 열의 리셋 (reset_index 메서드 )
+#
+
+# - 만약 멀티 index를 가지고 있다면, level 을 통해서 선택해서 리셋해줄수있다. 
+
+# + active=""
+# data.reset_index(
+#     level: 'Optional[Union[Hashable, Sequence[Hashable]]]' = None,
+#     drop: 'bool' = False,
+#     inplace: 'bool' = False,
+#     col_level: 'Hashable' = 0,
+#     col_fill: 'Label' = '',
+# ) -> 'Optional[DataFrame]'
+# -
+
+data.reset_index()
+
+# # pandas dataframe에 여러개의 빈열 추가
+
+data["B"] = None
+data["C"] = None
+data["D"] = None
+data
+
+data[["솔", "라", "시"]] = None
+data
+
+pd.concat([data,pd.DataFrame(columns=list('EFG'))])
+
+
+
+# ## Group by 기초
+
+# +
+# 한개 열을 기준으로 집계
+
+import pandas as pd
+df = pd.DataFrame({'상품번호' : ['P1', 'P1', 'P2', 'P2'],
+                   '수량' :     [2, 3, 5, 10]})
+# -
+
+df
+
+# - count, min, mean, sum, max ,cumsum 이 있다.
+
+# min: 상품번호별 판매된 최소 수량
+df.groupby(by=['상품번호'], as_index=False).min()  # as_index를 false로 해주면, 인덱스로 가지 않아서 , 활용이 편함
+
+# max: 상품번호별 판매된 최대 수량
+df.groupby(by=['상품번호'], as_index=False).max()
+
+# 여러개 열을 기준으로 집계할 떄
+import pandas as pd
+df = pd.DataFrame({'고객번호' : ['C1', 'C2', 'C2', 'C2'],
+                   '상품번호' : ['P1', 'P1', 'P2', 'P2'],
+                   '수량' :     [2, 3, 5, 10]})
+
+# sum: 고객별 발송해야 할 상품별 수량 합계
+df.groupby(by=['고객번호', '상품번호'], as_index=False).sum()
+
+# ## 조건에 따른 데이터 프레임 누적합 계산
+
+import pandas as pd 
+d = {'NAME': ['PIKACHU', 'GYARADOS', 'LAPRAS', 'Rattata', 'ZAPDOS'], 'NUM': [10, 250, 10, 20, 700], 'ENERGY' : [1, 1, 1, 1, 1]} 
+df = pd.DataFrame(data=d) 
+df
+
+
+# ### NUM이 10일 때까지 ENERGY의 값을 누적하여 저장
+# NUM이 10이 되는 순간 다시 cumsum을 reset하고 재 누적합 계산 시작
+#
+
+df['cumsum'] = df.groupby((df.NUM == 10).cumsum()).ENERGY.cumsum() 
+df
+
