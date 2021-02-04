04547568d986b13ab0e5172c118fefc4953aac2f
leehj01 <leehjung55@gmail.com>
2021-02-04 23:36:16 +0900

    commit

--- Python/03. Pandas Groupby.py
+++ Python/03. Pandas Groupby.py
@@ -38,10 +38,73 @@ df.groupby(by=['상품번호'], as_index=False).max()
 import pandas as pd
 df = pd.DataFrame({'고객번호' : ['C1', 'C2', 'C2', 'C2'],
                    '상품번호' : ['P1', 'P1', 'P2', 'P2'],
-                   '수량' :     [2, 3, 5, 10]})
+                   '수량' :     [2, 3, 5, 10],
+                  '포인트' : [100, 200, 100, 50 ]})
+
+df_grouped = df.groupby(['고객번호','상품번호'])['포인트'].sum()
+df_grouped
+
+df_grouped.index
+
+# - groupby 명령의 결과물도 결국 dataframe이기 때문에,
+# 두개의 column으로 gropbt할 경우, index가 두개 생성된다.
+
+# group으로 묶여진 데이터를 matrix형태로 전환해줌.
+df_grouped.unstack()
+
+# +
+# 인덱스 level을 변경할 수 있다.
+# -
+
+df_grouped.swaplevel()
+
+df_grouped.swaplevel().sortlevel(0)
+
+# +
+## 인덱스 level을 기준으로 기본연산 수행
+# -
+
+df_grouped.sum(level = 0)
+
+df_grouped.sum(level = 1)
 
 # sum: 고객별 발송해야 할 상품별 수량 합계
-df.groupby(by=['고객번호', '상품번호'], as_index=False).sum()
+df_grouped_2 = df.groupby(by=['고객번호', '상품번호'], as_index=False).sum()
+df_grouped_2
+
+df_grouped_2.unstack()  # value가 두개이기 때문에, 예쁘게 안떨어짐 
+
+# ## grouped 
+
+# ### groupby에 의해 split된 상태를 추출 가능함
+
+# +
+grouped = df.groupby('고객번호')
+
+for name, group in grouped:
+    print(name)
+    print(group)
+# -
+
+# ### 특정 key값을 가진 구룹의 정보만 추출 가능
+# - 추출된 group 정보에는 3가지 유형의 apply가 가능함
+#     - aggregation : 요약된 통계정보를 추출해줌
+#     - transformation : 해당정보를 변환해줌
+#     - filtration : 특정 정보를 제거하여 보여주는 필터링 가능
+
+grouped.get_group('C2')
+
+## 1. aggregation 
+grouped.agg(sum)
+
+import numpy as np
+grouped.agg(np.mean)
+
+# 특정 컬럼에 여러개의 function을 apply할 수 도있음
+grouped['포인트'].agg([np.sum, np.mean, np.std])
+
+## 2. trainsformation : agg와 달리 key값 별로 요약된 정보가 아니라, 개별 데이터의 변환을 지원함
+
 
 # ## 조건에 따른 데이터 프레임 누적합 계산
 
@@ -57,6 +120,7 @@ df
 
 df['cumsum'] = df.groupby((df.NUM == 10).cumsum()).ENERGY.cumsum() 
 df
+
 
 
 
