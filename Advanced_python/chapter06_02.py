75edb98aefa520a9952d9eb808cb00e6a8274626
HWAJUNG LEE <65941186+leehj01@users.noreply.github.com>
2021-08-14 23:52:27 +0900

    Update chapter06_02.py

--- Advanced_python/chapter06_02.py
+++ Advanced_python/chapter06_02.py
@@ -5,20 +5,20 @@
 # 코루틴(Coroutine)
 
 # yield : 메인 루틴 <-> 서브 루틴
-   #  yield 를 만나면 일단 멈쳐있다가, 다른 루틴을 실행했다가 next 를 실행하면 ,멈쳐있는 것이 실행
+#  yield 를 만나면 일단 멈쳐있다가, 다른 루틴을 실행했다가 next 를 실행하면 ,멈쳐있는 것이 실행
 # 코루틴 제어, 코루틴 상태, 양방향 값 전송
 # yield from
 
 # 서브루틴 : 메인루틴에서 -> 리턴에 의해 호출 부부느올 돌아와 다시 프로세스 시작
 # 코루틴 : 루틴 실행중 멈춤가능 -> 특정 위치로 돌아갔다가 -> 다시 원래 위치로 돌아와 수행을 가능하게 함. -> 동시성을 프로그래밍을 가능하게 해줌
-    # 코루틴으로 generator 을 한 걸 열어보면, NEXT 를 하면, 다시 실행되는 동시성이 가능함.
-    # 메인루틴은 진입, 탄출구가 하나이지만, 코루틴은 여러방향으로 동시성을 프로그래밍을 가능하게 해줌
-    # 코루틴의 단점은 가독성이 떨어져서, 디버깅을 잘해야함. 하나의 쓰레드만 이용하기 때문에 , 어쩔수 없을 땐 멀티쓰레드를 사용해야함
-    # 코루틴은 스케쥴링 오버헤드가 매우 적다.! - 하나의 스레드에서 실행하기 때문에! ( 작은 메모리가 움직이기 때문에 오버헤드가 작다. )
+# 코루틴으로 generator 을 한 걸 열어보면, NEXT 를 하면, 다시 실행되는 동시성이 가능함.
+# 메인루틴은 진입, 탄출구가 하나이지만, 코루틴은 여러방향으로 동시성을 프로그래밍을 가능하게 해줌
+# 코루틴의 단점은 가독성이 떨어져서, 디버깅을 잘해야함. 하나의 쓰레드만 이용하기 때문에 , 어쩔수 없을 땐 멀티쓰레드를 사용해야함
+# 코루틴은 스케쥴링 오버헤드가 매우 적다.! - 하나의 스레드에서 실행하기 때문에! ( 작은 메모리가 움직이기 때문에 오버헤드가 작다. )
 # 쓰레드 : 지금까지는 싱글쓰레드로 했는데, 다음시간에는 멀테쓰레드로 함 -> 좀 복잡함 -> 그 이유는
 #     공유되는 자원에 대한 교착 상태 발생 가능성이 있다. 따라서, 주의 깊게 코드를 짜야한다. -> 컨텍스트  스위칭 비용 발생, 자원소비 가능성 증가
-    # 멀티 쓰레드나, 코루틴이나 잘 쓰면 상관 없음.
-    # 멀티 쓰레드는 컨텍스트 스위칭이라는 스케쥴링이기 때문에 좀 느려지는 경향이있다.
+# 멀티 쓰레드나, 코루틴이나 잘 쓰면 상관 없음.
+# 멀티 쓰레드는 컨텍스트 스위칭이라는 스케쥴링이기 때문에 좀 느려지는 경향이있다.
 
 # 코루틴 예제1
 def coroutine1():
@@ -26,9 +26,10 @@ def coroutine1():
     i = yield  # yield  i 를 쓰면 값을 반환만 할 수 있는데, i = yield 이렇게 선언하면, 메인 루틴한테 값을 받을 수 있음 - 양방향 값을 전송할 수 있음
     print('>>> coroutine received : {}'.format(i))
 
+
 # 제너레이터 선언
 c1 = coroutine1()
-print('ex 1-1 -', c1 , type(c1)) # <generator object coroutine1 at 0x000001FFAC033D48> <class 'generator'>
+print('ex 1-1 -', c1, type(c1))  # <generator object coroutine1 at 0x000001FFAC033D48> <class 'generator'>
 
 # yield 실행 전까지 진행
 # next(c1)
@@ -40,6 +41,7 @@ print('ex 1-1 -', c1 , type(c1)) # <generator object coroutine1 at 0x000001FFAC0
 
 # 잘못된 사용
 c2 = coroutine1()
+
 
 # next(c2) # 이걸 입력하지 않으면, 아래와 같은 TypeError 예외 발생하게 된다.
 # c2.send(100) # TypeError: can't send non-None value to a just-started generator
@@ -58,9 +60,10 @@ def coroutine2(x):
     print('>>> corountine received : {}'.format(z))
     # 왼쪽에 있는건 우리가 전달해줘야하는 값, 오른쪽은 우리한테 전송하는
 
+
 c3 = coroutine2(10)
 
-from inspect import getgeneratorstate # 상태값을 볼수 있는 패키지
+from inspect import getgeneratorstate  # 상태값을 볼수 있는 패키지
 
 print('EX 1-2 - ', getgeneratorstate(c3))
 
@@ -83,24 +86,28 @@ print()
 # next 메소드를 사용하지 않고 바로 실행하기 위해서, decorator 을 이용하
 
 # 데코레이터 패턴
-from functools import wraps # 나한테 전달받는 함수를 모두 wrap - 내부 attribute 의 것을 싸고 가겠다.
+from functools import wraps  # 나한테 전달받는 함수를 모두 wrap - 내부 attribute 의 것을 싸고 가겠다.
+
+
 def coroutine(func):
     '''    Decorator run until yield    '''
+
     @wraps(func)
     def primer(*args, **kwargs):
         gen = func(*args, **kwargs)
         next(gen)
         return gen
+
     return primer
 
+
 @coroutine
-def sumer(): # send 로 보내는 함수를 계속 더하는 함수
+def sumer():  # send 로 보내는 함수를 계속 더하는 함수
     total = 0
     term = 0
     while True:
         term = yield total
         total += term
-
 
 
 sum_ex = sumer()
@@ -114,11 +121,13 @@ print('ex 2-3 -', sum_ex.send(60))
 print()
 print()
 
+
 # 코루틴 예제 3 (예외 처리)
 # coroutine 의 예외를 줘서 중지시키기
 
 class SampleException(Exception):
     '''설명에 사용할 예외 유형'''
+
 
 def coroutine_except():
     print('>>> coroutine started')
@@ -134,10 +143,11 @@ def coroutine_except():
     finally:
         print('>>> coroutine ending')
 
+
 exe_co = coroutine_except()
 
 print('EX 3-1 - ', next(exe_co))
-print('EX 3-2 -', exe_co.send(10)) # 주기만 하고 받은게 없기 때문에 NONE
+print('EX 3-2 -', exe_co.send(10))  # 주기만 하고 받은게 없기 때문에 NONE
 print('EX 3-3 -', exe_co.send(100))
 print('EX 3-2 -', exe_co.throw(SampleException))  # 예외를 던짐 - 하지만,예외를 처리했기 때문에 끝나지 않았으므로 계속 실행 가능
 print('EX 3-3 -', exe_co.send(1000))
@@ -147,12 +157,13 @@ print('EX 3-3 -', exe_co.close())  # GEN_CLOSED  - 여기서는 끝남을 해주
 print()
 print()
 
+
 # 코루틴 예제 4 ( return )
 
 def averager_re():
     total = 0.0
-    cnt =0
-    avg =None
+    cnt = 0
+    avg = None
     while True:
         term = yield
         if term is None:
@@ -161,6 +172,7 @@ def averager_re():
         cnt += 1
         avg = total / cnt
     return 'Average : {}'.format(avg)
+
 
 avger2 = averager_re()
 
@@ -174,7 +186,69 @@ avger2.send(50)
 try:
     avger2.send(None)
 except StopIteration as e:
-    print('EX 4-1 -', e.value) # 리턴으로 반환하는 값은 예외 처리에서 가져오는 value값에서 확인할 수 있다.
+    print('EX 4-1 -', e.value)  # 리턴으로 반환하는 값은 예외 처리에서 가져오는 value값에서 확인할 수 있다.
 
 # 코루틴 처리는 내부적으로 패키지들이 많이 존재하기 때문에, 직접 구현할일은 없지만
 # 원리는 아는 것이 필요하다.
+
+# 코루틴 예제 5( yeild from ) - 3. 대에서 나온것
+# StopIteration 자동처리 (3.7  이후부터는 ->  await 로 바뀜)
+# 중첩 코루틴 처리
+
+def gen1(): # 중첩으로 하나의 코루틴 안에 들어갈 수 있음
+    for x in 'AB':
+        yield x
+    for y in range(1,4):
+        yield y
+
+t1 = gen1()
+
+print('EX 5-1 -', next(t1))
+print('EX 5-2 -', next(t1))
+print('EX 5-3 -', next(t1))
+print('EX 5-4 -', next(t1))
+print('EX 5-5 -', next(t1))
+# print('EX 5-6 -', next(t1))  # StopIteration
+
+t2 = gen1()
+
+print('EX 5-7 -', list(t2))
+
+print()
+print()
+
+def gen2():
+    yield from 'AB' # 처음과 끝을 자기가 알고있고, 제어를 해줌 yield from 에 위임
+    yield from range(1,4)
+
+t3 = gen2()  # for 문으로 하는 것을 간단하게, yield from 으로 만들 수 있다.
+print('EX 6-1 -', next(t3))
+print('EX 6-2 -', next(t3))
+print('EX 6-3 -', next(t3))
+print('EX 6-4 -', next(t3))
+print('EX 6-5 -', next(t3))
+
+t4 = gen2()
+
+print('EX 6-6 -', list(t4))
+print()
+print()
+
+# 최종 예제 - 메인과 서브 테스크 - 흐름 제어
+
+def gen3_sub():
+    print('Sub coroutine.')
+    x = yield 10
+    print('Recv : ', str(x))
+    x = yield 100
+    print('Recv : ', str(x))
+
+def gen4_main():
+    yield from gen3_sub() # gen3_sub를 위임
+    # 코루틴 끼리, yield from 을 통해서 흐름 제어를 할 수 있음
+
+t5 = gen4_main()
+
+print('EX 7-1 -', next(t5))
+print('EX 7-2 -', t5.send(7))
+print('EX 7-2 -', t5.send(77))
