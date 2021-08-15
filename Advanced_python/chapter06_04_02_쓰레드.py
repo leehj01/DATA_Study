68d698fa9547b001e06f046a9eb0414c3775a2b1
HWAJUNG LEE <leehjung55@gmail.com>
2021-08-16 00:05:35 +0900

    chapter06_04_02

--- Advanced_python/chapter06_04_02_쓰레드.py
+++ Advanced_python/chapter06_04_02_쓰레드.py
@@ -7,12 +7,14 @@
 # Non-Blocking 비동기 처리에 적합
 # 코루틴에는 Asyncio 가 존재
 
-# BlockIO : 내가 무엇가 요청할때, 쓰레드가 멈쳐있는 것. 한명때문에 모두가 멈쳐있는 것
-# 순차 실행
+# BlockIO -> Thread 사용
+# 쓰레드 개수 및 GIL 문제 염두, 공유 메모리 문제 해결
 
 import timeit
 from urllib.request import urlopen # 웹사이트에 요청
 import ssl
+from concurrent.futures import ThreadPoolExecutor
+import threading # 어떤 쓰레드가 어떻게 일하는지 보는 것 - 몇번쓰레드까지 만들어졌는지 모니터링
 
 # urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1108)>
 # 위와 같은 에러가 발생하여, 아래의 context 를 만들어주고, 그것을 uropen 에 넣어준다. 그럼 해결된다.
@@ -24,17 +26,26 @@ urls = ['http://daum.net', 'https://google.com', 'https://apple.com', 'https://t
 
 start = timeit.default_timer()
 
-# 순차 실행부
-for url in urls:
-    print('Strart', url)
-    #실제 요청
-    text = urlopen(url, context =context) # 요청이 오면, 아래에 print - 요청이 와야 다음 단계에 가기 때문에, 이것때문에 동시에 하지는 못함 요청하면 모든 코루틴이 멈춘다
-    # print('Contents', dir(text), text.read()) # text.read() : 실제 메인페이지의 내용을 볼수 있다.
-    # 이것을 block IO 라고 함
-    print('Done', url)
 
-# 완료시간 - 시작시간
-duration = timeit.default_timer() - start
+def fetch(url):
+    print('Thread Name :', threading.current_thread().getName(), 'Start', url) # 현재 쓰레드가 어디인지를 나타내는 애
+    urlopen(url, context=context)
+    print('Thread Name :', threading.current_thread().getName(), 'Done', url)  # 현재 쓰레드가 어디인지를 나타내는 애
 
-# 총 실행 시간
-print('Total Time', duration)  # Total Time 3.2365186959999996
+
+# 동시성이기 때문에, 실행하는 순서도 매번 바뀐다. 그냥 일이 끝나는데로 반환되는 것을 볼 수 있다. 
+def main():
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for url in urls:
+            executor.submit(fetch, url) # 패치 함수와 url 을 넣어주면 됨.
+
+# 쓰레드를 할때는 main을 늘 만들어줘야한다. 진입점이 없으면 에러가 발생한다.
+if __name__ == '__main__':
+    # 함수 실행
+    main()
+
+    # 완료시간 - 시작시간
+    duration = timeit.default_timer() - start
+
+    # 총 실행 시간
+    print('Total Time', duration) # Total Time 1.591328305
